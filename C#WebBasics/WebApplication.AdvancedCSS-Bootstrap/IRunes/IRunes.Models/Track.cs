888f9e10a82a0501558f15bb72808636d2f6bd35
iliyants <iliyan.lts@gmail.com>
2020-01-17 21:02:08 +0200

    added automatic routing

---
+++ C#WebBasics/WebApplication.AdvancedCSS-Bootstrap/IRunes/IRunes.Models/Track.cs
@@ -0,0 +1,23 @@
+﻿namespace IRunes.Models
+{
+    using System.ComponentModel.DataAnnotations;
+
+    public class Track
+    {
+        [Key]
+        public string Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string Link { get; set; }
+
+        [Required]
+        [Range(typeof(decimal), "0", "79228162514264337593543950335")]
+        public decimal Price { get; set; }
+
+        public string AlbumId { get; set; }
+        public Album Album { get; set; }
+    }
+}
